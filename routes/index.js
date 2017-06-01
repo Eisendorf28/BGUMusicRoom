58863a1dc568bb4f4856f3cffed9408377401347
Pavel <eisendorf28@gmail.com>
2017-06-01 14:11:56 +0300

    added nodemon

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', Pavel:"Pavel" });
+  res.render('index', { title: 'Express', Pavel:"Daniel" });
 });
 
 module.exports = router;
