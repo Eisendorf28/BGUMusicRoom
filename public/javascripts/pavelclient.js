962c05352941c68386fb96a37320693ce4953e3a
Pavel <eisendorf28@gmail.com>
2017-06-01 15:09:53 +0300

    added calendar

--- public/javascripts/pavelclient.js
+++ public/javascripts/pavelclient.js
@@ -1,3 +1,12 @@
 var j = 3;
 j = j + 3;
 window.j = j;
+$(document).ready(function() {
+
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+    })
+
+});
