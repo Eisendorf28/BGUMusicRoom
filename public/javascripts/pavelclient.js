c5bd8cde0f74d71239df6940f65d3b36e6a8f325
Pavel <eisendorf28@gmail.com>
2017-06-03 16:22:43 +0300

    More small changes to view

--- public/javascripts/pavelclient.js
+++ public/javascripts/pavelclient.js
@@ -1,19 +1,33 @@
-var j = 3; //just a function to show that clients can also process.
-j = j + 3;
-window.j = j;
-$(document).ready(function() {
+// var j = 3; //just a function to show that clients can also process.
+// j = j + 3;
+// window.j = j;
+$(document).ready(function () {
 
     // page is now ready, initialize the calendar...
-  //$('#calendar').fullCalendar('next'); //what does it do?
-   $('#calendar').fullCalendar({
-        defaultView: 'agendaWeek',
-        duration: {days: 7},
-        dayCount: {days: 7}
-   });
+    //$('#calendar').fullCalendar('next'); //what does it do?
     $('#calendar').fullCalendar({
-        dayClick: function() {
-        //alert('a day has been clicked!');
-        console.log('adsdadsa');}
-    })
-   // $('#calendar'.fullCalendar.agendaWeek);
+        defaultView: 'agendaWeek',
+        // duration: {days: 1} // doesn't work
+        header: { 
+            center: 'month,agendaWeek'}, // buttons for switching between views
+        views: {
+            agendaWeek: {
+            type: 'agenda',
+            duration: { days: 7 },
+            buttonText: 'Week'
+        }
+        // ,
+        // month: {
+        //     type: 'month',
+        //     duration: { days: 30},
+        //     buttonText: 'Month'
+        // }
+    }
+    });
+    // $('#calendar').fullCalendar({
+    //     dayClick: function() {
+    //     //alert('a day has been clicked!');
+    //     console.log('adsdadsa');}
+    // })
+    // $('#calendar'.fullCalendar.agendaWeek);
 });
