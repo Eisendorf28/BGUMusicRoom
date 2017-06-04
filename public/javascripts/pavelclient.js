e48cc1e400a0ee37fa4be313ecd7ec021372b94e
Pavel <eisendorf28@gmail.com>
2017-06-04 14:20:28 +0300

    added selections

--- public/javascripts/pavelclient.js
+++ public/javascripts/pavelclient.js
@@ -1,23 +1,28 @@
 // var j = 3; //just a function to show that clients can also process.
 // j = j + 3;
 // window.j = j;
-
+// var nowISO = moment().toISOString();
+// var now = moment();
+// document.write(now);
 //Should I delete entries every time or save them? If i delete them - i need to use visibleRange to restrict former weeks.
 $(document).ready(function () {
-
     // page is now ready, initialize the calendar...
     $('#calendar').fullCalendar({
         defaultView: 'agendaWeek',
         slotDuration: '01:00:00',
         allDaySlot: false,
-        slotLabelFormat: 'hh:mm',
-        slotEventOverlap: false,
+        slotLabelFormat: 'HH:mm',
+        slotEventOverlap: false,                //what if drums/piano are taken away?   
         nowIndicator: true,
-        // duration: {days: 1} // doesn't work
+        displayEventTime: true,                 //on events
+        displayEventEnd: true,                 //same 
+        //dayPopoverFormat,                     //change later
+        selectable: true,
+        selectHelper: true,
         header: {
-            center: 'agendaWeek',       //, more views 
-            left: 'title',              //the date title
-            right: 'walla today prev,next'    //today, prev and next 
+            center: 'agendaWeek',               //, more views 
+            left: 'title',                      //the date title
+            right: 'walla today prev,next'      //today, prev and next 
         },
         // buttons for switching between views
         views: {
@@ -31,12 +36,14 @@ $(document).ready(function () {
             //alert('a day has been clicked!');
             console.log('a day has been clicked!');
         },
-        validRange: function (nowDate) {
-            return {
-                start: nowDate,                         //not good, need to make it 1 hour from NOW
-                end: nowDate.clone().add(3, 'days')
-            };
-        },
+        // validRange: function (nowDate) {             //only works on dates and not hours apparently
+        //     console.log(now);
+        //     console.log(nowDate);
+        //     return {
+        //         start: now,                         //not good, need to make it 1 hour from NOW
+        //         end: nowDate.clone().add(3, 'days')
+        //     };
+        // },
         customButtons: {
             walla: {    //the name of my custom button
                 text: 'custom!',
