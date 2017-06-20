ab557c6c6e250d8c911838e5ad5c38b75babdc9d
Pavel <eisendorf28@gmail.com>
2017-06-20 17:41:39 +0300

    event creation #1

--- public/javascripts/pavelclient.js
+++ public/javascripts/pavelclient.js
@@ -3,23 +3,53 @@
 // window.j = j;
 var now = moment().add(3, 'hours');
 var nowISO = now.toISOString();
-var laterISO = now.clone().add(3 , 'days').toISOString();
-console.log(nowISO + " " + laterISO);
-// document.write(now);
+var laterISO = now.clone().add(3, 'days').toISOString();
+//console.log(nowISO + " " + laterISO);
+
 //Should I delete entries every time or save them? If i delete them - i need to use visibleRange to restrict former weeks.
-var businessHours = {// days of week. an array of zero-based day of week integers (0=Sunday)
-    dow: [1, 2, 3, 4], // Monday - Thursday
-    start: '10:00', // a start time (10am in this example)
-    end: '12:00' // an end time (6pm in this example)
-};
+// var businessHours = {// days of week. an array of zero-based day of week integers (0=Sunday)
+//     dow: [1, 2, 3, 4], // Monday - Thursday
+//     start: '10:00', // a start time (10am in this example)
+//     end: '12:00' // an end time (6pm in this example)
+// };
+//selectInfo = this.selectInfo;
+// var selected = function (selectInfo) {
+//     console.log(selectInfo);
+//     return selectInfo;
+// }
+var event = { id: 1, title: 'New event', start: nowISO, end: laterISO };
+
+$('#calendar').fullCalendar('renderEvent', event, true);                    //change
 var selectValidation = function (selectInfo) {
-    //console.log(selectInfo);
+    //console.log(selectInfo.end.title);
     var duration = moment.duration(selectInfo.end.diff(selectInfo.start));
     var hours = duration.asHours();
-    console.log(hours);
+    //console.log(hours);
     //  debugger;
     return (hours < 3);                         //what if admin account?
 }
+
+// var eventsObject = function addCalanderEvent(selectInfo) {
+//     var eventObject = {
+//         //title: title,
+//         start: selectInfo.start,
+//         end: selectInfo.end,
+//         //id: id, 
+//         //color: colour
+//     };
+//     console.log(selectInfo.start.toISOString);
+//     $('#calendar').fullCalendar('renderEvent', eventObject, true);
+//     return eventObject;
+// }
+// var newEvent = new Object();
+// newEvent.title = "alright";
+// newEvent.start = selectInfo;
+// $('#calendar').fullCalendar( 'renderEvent', newEvent );
+// console.log(newEvent.title);
+// console.log(newEvent.start);
+// var singleEvent = function (selectInfo) {
+//     console.log("The chosen hours are: " + selectInfo.start + "until " + selectInfo.end);
+//     //console.log(selectInfo());
 $(document).ready(function () {
     // page is now ready, initialize the calendar...
     $('#calendar').fullCalendar({
@@ -37,9 +67,10 @@ $(document).ready(function () {
         selectOverlap: false,
         selectConstraint: {
             start: nowISO,                     //no duration function
-            end: laterISO 
+            end: laterISO
         },
         selectAllow: selectValidation,
+
         // eventConstraint: {                  //no duration function yet
         //     businessHours
         // },
@@ -56,18 +87,12 @@ $(document).ready(function () {
                 buttonText: 'Week',
             }
         },
-        dayClick: function () {
-            //alert('a day has been clicked!');
-            console.log('a day has been clicked!');
-        },
-        // validRange: function (nowDate) {             //only works on dates and not hours apparently
-        //     console.log(now);
-        //     console.log(nowDate);
-        //     return {
-        //         start: now,                         //not good, need to make it 1 hour from NOW
-        //         end: nowDate.clone().add(3, 'days')
-        //     };
-        // },
+        // dayClick: function (selectInfo) {
+        //     //console.log(selectInfo);
+        //     var eventObject = { title: "crap", start: selectInfo.start.format(), end: selectInfo.end.format() };
+        //     $('#calendar').fullCalendar('renderEvent', eventObject, true);
+
+        //     console.log('a day has been clicked!');
         customButtons: {
             walla: {    //the name of my custom button
                 text: 'custom!',
@@ -76,5 +101,6 @@ $(document).ready(function () {
                 }
             }
         },
+
     })
 });
