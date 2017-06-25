195cec351683666d7c1400883e678105060988d5
Pavel <eisendorf28@gmail.com>
2017-06-26 01:06:13 +0300

    minor changes

--- public/javascripts/pavelclient.js
+++ public/javascripts/pavelclient.js
@@ -1,56 +1,14 @@
-// var j = 3; //just a function to show that clients can also process.
-// j = j + 3;
-// window.j = j;
 var now = moment().add(3, 'hours');
 var nowISO = now.toISOString();
 var laterISO = now.clone().add(3, 'days').toISOString();
-var selection = [null, null];
+var selection = [null, null];                       //array holding selected info selection[start_timestamp][end_timestamp]
 console.log(selection[0] + selection[1]);
-//console.log(nowISO + " " + laterISO);
-//Should I delete entries every time or save them? If i delete them - i need to use visibleRange to restrict former weeks.
-// var businessHours = {// days of week. an array of zero-based day of week integers (0=Sunday)
-//     dow: [1, 2, 3, 4], // Monday - Thursday
-//     start: '10:00', // a start time (10am in this example)
-//     end: '12:00' // an end time (6pm in this example)
-// };
-//selectInfo = this.selectInfo;
-// var selected = function (selectInfo) {
-//     console.log(selectInfo);
-//     return selectInfo;
-// }
-var nowISO2 = moment().add(7, 'hours').toISOString();
-var laterISO2 = moment().add(10, 'hours').toISOString();
-//change
-var selectValidation = function (selectInfo) {
-    //console.log(selectInfo.end.title);
+var selectValidation = function (selectInfo) {      //restricts selection for 2 hours tops
     var duration = moment.duration(selectInfo.end.diff(selectInfo.start));
     var hours = duration.asHours();
-    //console.log(hours);
     //  debugger;
     return (hours < 3);                         //what if admin account?
 }
-//var event = { id: 1, title: 'New event', start: nowISO2, end: laterISO2 };
-// var eventsObject = function addCalanderEvent(selectInfo) {
-//     var eventObject = {
-//         //title: title,
-//         start: selectInfo.start,
-//         end: selectInfo.end,
-//         //id: id, 
-//         //color: colour
-//     };
-//     console.log(selectInfo.start.toISOString);
-//     $('#calendar').fullCalendar('renderEvent', eventObject, true);
-//     return eventObject;
-// }
-// var newEvent = new Object();
-// newEvent.title = "alright";
-// newEvent.start = selectInfo;
-// $('#calendar').fullCalendar( 'renderEvent', newEvent );
-// console.log(newEvent.title);
-// console.log(newEvent.start);
-// var singleEvent = function (selectInfo) {
-//     console.log("The chosen hours are: " + selectInfo.start + "until " + selectInfo.end);
-//     //console.log(selectInfo());
 $(document).ready(function () {
     // page is now ready, initialize the calendar...
     $('#calendar').fullCalendar({
@@ -58,7 +16,7 @@ $(document).ready(function () {
         slotDuration: '01:00:00',
         allDaySlot: false,                      //shit
         slotLabelFormat: 'HH:mm',
-        slotEventOverlap: false,               //what if drums/piano are taken away?   
+        slotEventOverlap: false,               //what if drums/piano are taken away?   irrelevant nw
         nowIndicator: true,
         displayEventTime: true,                //on events
         displayEventEnd: true,                 //same
@@ -80,7 +38,7 @@ $(document).ready(function () {
             left: 'title',                      //the date title
             right: 'walla today prev,next'      //today, prev and next 
         },
-        // buttons for switching between views
+        // buttons for switching between views no need
         views: {
             agendaWeek: {
                 type: 'agenda',
@@ -258,8 +216,8 @@ function destroyAndRender() {
     });
 };
 render();
-function render() {
-    $.ajax({    //rendering all events from database
+function render() {             //rendering all events from database
+    $.ajax({    
         type: 'GET',
         url: '/API/room_occupation/all',
         success: function (event) {
